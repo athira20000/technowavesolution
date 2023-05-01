3a8dc05e793c2fdca14a2d4ab6c3069b2b8728dc
athira20000 <132265156+athira20000@users.noreply.github.com>
2023-05-01 22:03:24 +0530

    Add files via upload
    this is create for education purpose

---
+++ vulnerable.php
@@ -0,0 +1,32 @@
+<?php
+$servername = "localhost";
+$username = "username";
+$password = "password";
+$dbname = "myDB";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+// Get input from user
+$input = $_GET['input'];
+
+// Execute SQL query
+$sql = "SELECT * FROM myTable WHERE name = '" . $input . "'";
+$result = $conn->query($sql);
+
+// Display results
+if ($result->num_rows > 0) {
+  // output data of each row
+  while($row = $result->fetch_assoc()) {
+    echo "id: " . $row["id"]. " - Name: " . $row["name"]. "<br>";
+  }
+} else {
+  echo "0 results";
+}
+$conn->close();
+?>
